c234cf203d8deaede21dc52bc2d0ec542e760803
Razvan Timis <timisrazvanvasile@gmail.com>
2021-05-12 20:32:00 +0300

    Add

--- 04 Week - js & dom/jsbasic/array-object.js
+++ 04 Week - js & dom/jsbasic/array-object.js
@@ -31,7 +31,7 @@ console.log("FullName:", persoan.fullName)
 console.log("LastName:", persoan.fullName.lastName)
 console.log("First letter of lastName:", persoan.fullName.lastName[0])
 
-// atribuire
+// atribuire pe object
 persoan.isMan = false;
 console.log("Person.isMan=", persoan.isMan)
 
@@ -44,4 +44,30 @@ delete persoan.myNewKey;
 console.log("Delete new key", persoan)
 
 
-// Array
+// Array (lista, sir)
+
+var numbers = [1, 2, 3, 0, 3, 2, 5, 9] // o lista de numere
+var strings = ["paine", "apa", "lapte"] // o lista de string
+
+
+var array1 = [ // putem avea orice valorii
+  1, // index = 0
+  "paine", // index = 1
+  false,
+  null,
+  undefined,
+  {}, //  = obiect gol
+  { name: "razvan" },
+  [1, 2, 3]
+]
+
+// cum accesam? Dupa index
+
+console.log("object razvan=", array1[6])
+
+// cum aflam lungimea unui array?
+console.log("Cate element avem un array =", array1.length)
+
+// atribuire pe array
+array1[6] = false;
+console.log("object razvan modificat=", array1[6])
