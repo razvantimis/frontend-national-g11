2c3447e9f3e4db9a080b83f8abb772e29a461db3
Razvan Timis <timisrazvanvasile@gmail.com>
2021-06-14 20:57:12 +0300

    Add

--- 08 week /workshop/home.js
+++ 08 week /workshop/home.js
@@ -13,7 +13,8 @@ const displayPosts = (posts) => { // merge si cu function sa dam la then
   const containerListPost = document.getElementById('list-post');
   for (let index = 0; index < posts.length; index++) {
     const currentPost = posts[index];
-    const postHTML = createPostHTML(currentPost.title, currentPost.text);
+    const postHTML = createPostHTML(currentPost.id, currentPost.title, currentPost.text);
+
     containerListPost.appendChild(postHTML);
   }
 }
@@ -25,7 +26,7 @@ getPosts()
   });
 
 // Creaza html pentru un singur post
-function createPostHTML(title, text) {
+function createPostHTML(id, title, text) {
   const p = document.createElement('p');
   const h1 = document.createElement('h1');
   const article = document.createElement('article');
@@ -36,9 +37,16 @@ function createPostHTML(title, text) {
   p.innerText = text.length > 150 ? text.substring(0, 150) + "..." : text;
   h1.innerText = title;
 
+  const editLink = document.createElement('a');
+  editLink.href = `./edit-post.html?id=${id}`;
+  editLink.innerText = 'Edit'
+  article.appendChild(editLink);
+
   return article;
 }
 
+
+// test code
 function testCreatePostHTML() {
   const postDemoHTML = createPostHTML("titlu 1", "descrierea postului")
   console.log("createPostHTML() =", postDemoHTML)
