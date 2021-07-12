36c400a14115a273fcd7eba44f6fdaeb7a969025
Razvan Timis <timisrazvanvasile@gmail.com>
2021-07-12 20:52:36 +0300

    Add

--- 13 week/readme.md
+++ 13 week/readme.md
@@ -5,7 +5,7 @@ Links:
 https://developer.mozilla.org/en-US/docs/Web/JavaScript/Guide/Modules
 https://javascript.info/modules-intro
 2. Node - npm - https://nodejs.org/en/
-3. Webpack - https://webpack.js.org/
+3. Webpack - https://webpack.js.org/ - Un Tool pentru code de js
 4. SPA
 
 # Continuare Workshop Player
---
+++ extra/webpack-demo/webpack.config.prod.js
@@ -0,0 +1,22 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+module.exports = {
+  entry: './src/index.js',
+  mode: 'production',
+  output: {
+    filename: 'main.bundle.js',
+    clean: true,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader"],
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: 'Development',
+    }),
+  ],
+};
