4232458ac38f95e76f6151433718b69e686bf8e4
Razvan Timis <timisrazvanvasile@gmail.com>
2021-07-12 19:44:58 +0300

    Add

---
+++ 13 week/node-js/app.js
@@ -0,0 +1,11 @@
+// Ce este Node.Js?
+// Ne ajuta sa face backend (nu interectioneaza end userul cu app-ul) in javascript
+// Ne permite sa rulam codul de javascript fara browser ( nu avem acces la document - DOM Api)
+
+console.log('hello I\'m node')
+console.log(100+34)
+
+// Il putem rula prin terminal rulam: node app.js
+
+// Ce este npm?
+// O aplicatie scrisa in javascript care se ocupa de gestionarea packages
--- 13 week/readme.md
+++ 13 week/readme.md
@@ -4,7 +4,7 @@
 Links: 
 https://developer.mozilla.org/en-US/docs/Web/JavaScript/Guide/Modules
 https://javascript.info/modules-intro
-2. Node - https://nodejs.org/en/
+2. Node - npm - https://nodejs.org/en/
 3. Webpack - https://webpack.js.org/
 4. SPA
 
