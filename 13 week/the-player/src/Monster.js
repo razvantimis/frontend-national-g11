4a7acc24b8282ffb1ecc9a3ffd018c4b1fdf575f
Razvan Timis <timisrazvanvasile@gmail.com>
2021-07-14 19:35:40 +0300

    Add

--- 13 week/the-player/src/Monster.js
+++ 13 week/the-player/src/Monster.js
@@ -1,29 +1,29 @@
 
-class Monster {
-  static STEP = 10;
-  constructor(type) {
-    this.type = type;
-  }
 
-  static create(type, top, left) {
+import MoveHtmlElement from './MoveHtmlElement'
+class Monster extends MoveHtmlElement {
+  static STEP = 10;
+
+  // Creaza prezentarea in html a unui monstru
+  static create(type) {
     const imgMonster = document.createElement('img')
     imgMonster.src = `./assets/monster/monster${type}.svg`;
     imgMonster.style.width = "40px";
-    imgMonster.style.position = "absolute";
-    imgMonster.style.top = top + "px";
-    imgMonster.style.left = left + "px";
+   
     return imgMonster;
   }
 
+  constructor(type) {
+    super(Monster.STEP, Monster.create(type))
+    this.type = type;
+  }
+
   show(map, top, left) {
-    this.monster = Monster.create(this.type, top, left);
-    map.append(this.monster)
-  }
+    this.htmlElement.style.top = top + "px";
+    this.htmlElement.style.left = left + "px";
 
-  moveUp() {
-  
+    map.append(this.htmlElement)
   }
-
 }
 
 export default Monster;
