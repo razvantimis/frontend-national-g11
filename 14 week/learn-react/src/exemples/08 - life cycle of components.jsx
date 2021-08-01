abcdce2528f3c7c9639cba1d35648cbe48c99803
Razvan Timis <timisrazvanvasile@gmail.com>
2021-08-01 15:36:09 +0300

    Add

--- 14 week/learn-react/src/exemples/08 - life cycle of components.jsx
+++ 14 week/learn-react/src/exemples/08 - life cycle of components.jsx
@@ -2,6 +2,43 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 
+// Life cycle - ciclu de viata a unei componenta din prespectiva afisari ei in UI
+
+class MyComponent extends React.Component {
+
+
+  // 0. Se creeaza componenta => se apeleaza constructorul
+  constructor(props) {
+    super(props);
+    // orice cod scris aici se executa prima data
+  }
+
+  // 1. Componenta apare in UI ( se apeleaza automat de catre react)
+  componentDidMount(){
+    // se apeleaza dupa ce a fost afisata (doar odata se apeleaza)
+    // De exemplu fetch la server
+    // Exemple 2: setInterval
+  }
+
+  // 2. Componenta dispare din UI
+  componentWillUnmount(){
+    // se apeleaza inaite ca componenta sa dispara
+    // Exemple 2: clean pe setInterval
+  }
+
+  componentDidUpdate(nextProps, nextState) {
+
+  }
+
+
+
+  render() {
+
+    return (
+      <div>test</div>
+    )
+  }
+}
 
 
 // 1. Digital timer ( setInterval)
