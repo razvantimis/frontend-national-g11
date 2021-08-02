ba26f756584551b11dc16a88f27126a0f58cedd7
Razvan Timis <timisrazvanvasile@gmail.com>
2021-08-02 20:58:23 +0300

    Add Product
    Co-authored-by: AdiCotirla <AdiCotirla@users.noreply.github.com>
    Co-authored-by: rekasz1 <rekasz1@users.noreply.github.com>
    Co-authored-by: alinaanghelescua <alinaanghelescua@users.noreply.github.com>

---
+++ 15 week/my-shop/src/components/Product/Product.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import './Product.css';
+
+const Product = (props) => {
+
+  return (
+    <div className="product">
+      <img src={props.image} width="150" />
+      <h2>{props.title}</h2>
+      <h3>Price:{props.price}</h3>
+    </div>
+  )
+}
+
+export default Product;
+
--- 15 week/my-shop/src/pages/Home/Home.jsx
+++ 15 week/my-shop/src/pages/Home/Home.jsx
@@ -1,11 +1,45 @@
 import React from 'react';
 import './Home.css';
+import Product from '../../components/Product'
+// https://fakestoreapi.com/
 
+const product1 = {
+  "id": 1,
+  "title": "Fjallraven - Foldsack No. 1 Backpack, Fits 15 Laptops",
+  "price": 109.95,
+  "description": "Your perfect pack for everyday use and walks in the forest. Stash your laptop (up to 15 inches) in the padded sleeve, your everyday",
+  "category": "men's clothing",
+  "image": "https://fakestoreapi.com/img/81fPKd-2AYL._AC_SL1500_.jpg"
+}
 const Home = () => {
 
   return (
-    <div>
-      home page
+    <div className="home">
+      <Product title="title1" price={100} image={product1.image} />
+      <Product title="title2" price={200} image="https://fakestoreapi.com/img/81fPKd-2AYL._AC_SL1500_.jpg" />
+      <Product title="title3" price={300} image="https://fakestoreapi.com/img/81fPKd-2AYL._AC_SL1500_.jpg" />
+      <Product title="title3" price={300} image="https://fakestoreapi.com/img/81fPKd-2AYL._AC_SL1500_.jpg" />
+      <Product title="title3" price={300} image="https://fakestoreapi.com/img/81fPKd-2AYL._AC_SL1500_.jpg" />
+      <Product title="title3" price={300} image="https://fakestoreapi.com/img/81fPKd-2AYL._AC_SL1500_.jpg" />
+      <Product title="title3" price={300} image="https://fakestoreapi.com/img/81fPKd-2AYL._AC_SL1500_.jpg" />
+      <Product title="title3" price={300} image="https://fakestoreapi.com/img/81fPKd-2AYL._AC_SL1500_.jpg" />
+      <Product title="title3" price={300} image="https://fakestoreapi.com/img/81fPKd-2AYL._AC_SL1500_.jpg" />
+      <Product title="title3" price={300} image="https://fakestoreapi.com/img/81fPKd-2AYL._AC_SL1500_.jpg" />
+      <Product title="title3" price={300} image="https://fakestoreapi.com/img/81fPKd-2AYL._AC_SL1500_.jpg" />
+      <Product title="title3" price={300} image="https://fakestoreapi.com/img/81fPKd-2AYL._AC_SL1500_.jpg" />
+      <Product title="title3" price={300} image="https://fakestoreapi.com/img/81fPKd-2AYL._AC_SL1500_.jpg" />
+      <Product title="title3" price={300} image="https://fakestoreapi.com/img/81fPKd-2AYL._AC_SL1500_.jpg" />
+      <Product title="title3" price={300} image="https://fakestoreapi.com/img/81fPKd-2AYL._AC_SL1500_.jpg" />
+      <Product title="title3" price={300} image="https://fakestoreapi.com/img/81fPKd-2AYL._AC_SL1500_.jpg" />
+      <Product title="title3" price={300} image="https://fakestoreapi.com/img/81fPKd-2AYL._AC_SL1500_.jpg" />
+      <Product title="title3" price={300} image="https://fakestoreapi.com/img/81fPKd-2AYL._AC_SL1500_.jpg" />
+      <Product title="title3" price={300} image="https://fakestoreapi.com/img/81fPKd-2AYL._AC_SL1500_.jpg" />
+      <Product title="title3" price={300} image="https://fakestoreapi.com/img/81fPKd-2AYL._AC_SL1500_.jpg" />
+      <Product title="title3" price={300} image="https://fakestoreapi.com/img/81fPKd-2AYL._AC_SL1500_.jpg" />
+      <Product title="title3" price={300} image="https://fakestoreapi.com/img/81fPKd-2AYL._AC_SL1500_.jpg" />
+      <Product title="title3" price={300} image="https://fakestoreapi.com/img/81fPKd-2AYL._AC_SL1500_.jpg" />
+      <Product title="title3" price={300} image="https://fakestoreapi.com/img/81fPKd-2AYL._AC_SL1500_.jpg" />
+      <Product title="title3" price={300} image="https://fakestoreapi.com/img/81fPKd-2AYL._AC_SL1500_.jpg" />
     </div>
   )
 }
