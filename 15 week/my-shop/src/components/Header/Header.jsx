78ded67bdf0e7542f44dc167eaac163326ebb84f
Razvan Timis <timisrazvanvasile@gmail.com>
2021-08-02 20:29:18 +0300

    Add
    Co-authored-by: AdiCotirla <AdiCotirla@users.noreply.github.com>
    Co-authored-by: rekasz1 <rekasz1@users.noreply.github.com>
    Co-authored-by: alinaanghelescua <alinaanghelescua@users.noreply.github.com>

--- 15 week/my-shop/src/components/Header/Header.jsx
+++ 15 week/my-shop/src/components/Header/Header.jsx
@@ -4,10 +4,17 @@ import './Header.css';
 const Header = () => {
 
   return (
-    <div>
-      Header
+    <div className="header-container">
+      <h1>MyLogo</h1>
+      <nav>
+        <a href="/"><button>Home</button></a>
+        <a href="/myCart"><button>MyCart</button></a>
+      </nav>
     </div>
   )
 }
 
 export default Header;
+
+
+// 2 link uri 1 logo
