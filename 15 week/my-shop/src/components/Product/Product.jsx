2be3dbc864fc92d4721b6dae39438158dfc9de1f
Razvan Timis <timisrazvanvasile@gmail.com>
2021-08-04 20:55:08 +0300

    Add

--- 15 week/my-shop/src/components/Product/Product.jsx
+++ 15 week/my-shop/src/components/Product/Product.jsx
@@ -4,8 +4,8 @@ import './Product.css';
 const Product = (props) => {
 
   return (
-    <div className="product">
-      <img src={props.image} width="150" />
+    <div className="product" style={props.style}>
+      <img src={props.image} width={props.width || '150'} />
       <h2>{props.title}</h2>
       <h3>Price:{props.price} Lei</h3>
     </div>
--- 15 week/my-shop/src/pages/Cart/Cart.jsx
+++ 15 week/my-shop/src/pages/Cart/Cart.jsx
@@ -1,10 +1,51 @@
 import React from 'react';
+import Product from '../../components/Product'
+//======== mock data ========
+const product1 = {
+    id: 1,
+    title: "Fjallraven - Foldsack No. 1 Backpack, Fits 15 Laptops",
+    price: 109.95,
+    image: "https://fakestoreapi.com/img/81fPKd-2AYL._AC_SL1500_.jpg",
+    quantity: 2,
+}
+const productList = [product1, product1, product1, product1, product1];
+//======== mock data ========
+
+function getTotalPrice(productList) {
+    let totalPrice = 0;
+    productList.forEach(product => {
+        totalPrice = totalPrice + product.price * product.quantity;
+    })
+
+    return totalPrice;
+}
 
 const Cart = (props) => {
+    const total = getTotalPrice(productList)
 
     return (
         <div className="cart">
-            Pagina cart
+            <h1>List de produse</h1>
+            {productList.map(productItem => {
+                return (
+                    <div style={{ display: 'flex' }}>
+                        <Product
+                            style={{
+                                display: 'flex',
+                                justifyContent: 'space-evenly',
+                                marginRight: 20
+                            }}
+                            width={50}
+                            title={productItem.title}
+                            price={productItem.price}
+                            image={productItem.image}
+                        />
+                        <h2>X {productItem.quantity}</h2>
+                    </div>
+                )
+            })}
+            <h2>Total: {total} lei</h2>
+            <button>Trimite comanda</button>
         </div>
     )
 }
